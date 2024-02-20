95c6dda625132d9926bd27126a369592b93f580f
ArnavU <arnavumarkar3@gmail.com>
2024-02-20 22:59:48 +0530

    accessing origin from env variables

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
     // origin: [process.env.FRONTEND_URL],
-    origin: 'https://todo-app-frontend-xi.vercel.app',
+    origin: process.env.FRONTEND_URL.split(','),
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }))
