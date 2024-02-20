8a84ca39d7a29373e0789ba9a739f81f52d2d7db
ArnavU <arnavumarkar3@gmail.com>
2024-02-20 21:12:03 +0530

    origin changed to https://todo-app-frontend-xi.vercel.app/

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
     // origin: [process.env.FRONTEND_URL],
-    origin: 'http://localhost:5173',
+    origin: 'https://todo-app-frontend-xi.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }))
